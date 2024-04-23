22ce7e9362cd79e0577b7341ba0487969bab65b7
Talifhani Manyelenyele <20231686@ctucareer.co.za>
2024-04-23 13:27:11 +0200

    Add project files.

---
+++ Interface1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+public interface IPlayable
+{
+	void Play();
+}
+public class Guitar : IPlayable
+{
+	void IPlayable.Play()
+	{
+		Console.WriteLine("Playing the guitar...");
+	}
+}
+public class Piano : IPlayable
+{
+	void IPlayable.Play()
+	{
+		Console.WriteLine("Playing the piano...");
+	}
+}
+class Program
+{
+	static void Main(string[] args)
+	{
+		Guitar guitar = new Guitar();
+		Piano piano = new Piano();
+		((IPlayable)guitar).Play();
+		((IPlayable)piano).Play();
+	}
+}
